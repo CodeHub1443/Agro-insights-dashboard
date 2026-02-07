ae866b8a59838b1b70ed148f8ce42f1259c45247
CodeHub1443 <seriocomic123@gmail.com>
2026-02-07 16:37:23 +0600

    feat: setup github actions for dashboard deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Agro-insights-dashboard/",
   server: {
     host: "::",
     port: 8080,
